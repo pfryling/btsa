75d1af2676576ecf4cb56bfdb25f83f069f38e83
Paul Fryling <pfryling@gmail.com>
2014-12-06 03:00:34 -0800

    updated comments

--- js/btsa/services/btsaService.js
+++ js/btsa/services/btsaService.js
@@ -1,29 +1,33 @@
 window.biotechnology.service('btsaDataService', ['$http', '$q',
-    function($http, $q) {
+            function($http, $q) {
 
-        //This json object contains all the infomation used thoughout the website.
-        // Update meeting info and 
-        var clubInfo = {
-            "officers": {
-                "president": {
-                    "name": "Jenny Waters",
-                    "email": "jwater@gmail.com"
-      
-                },
-                "vice": "Harry Ho",
-                "tresurer": "Paul Fryling"
-            },
-            "meetinginfo": {
-                //These values get passed by the scope in our controller to the 
-                "time": "Monday at 5:30",
-                "location": "HH-130",
-                "semester": "Spring 2015"
-            }
+                //This json object (http://www.w3schools.com/json/) contains the infomation that may need to be updated thoughout the website.
+                //The values below are sent to our controller when the getInfo function is called by our service (https://docs.angularjs.org/guide/services).
+                // Through the "magic" of two way data-binding (https://docs.angularjs.org/tutorial/step_04) the information below will be passed through the scope to our home template.
+                /////////////////////////////////
+                ///////Update content here//////
+                ///////////////////////////////
+                var clubInfo = {
+                    "BTSA": {
+                        "president": {
+                            "name": "Jenny Waters",
+                            "email": "jwater@gmail.com"
 
-        };
+                        },
+                        "vice": {
+                            "name": "Harry Ho",
+                            "email": "h3penguin@gmail.com"
+                        },
+                        "meetinginfo": {
+                            "time": "Monday at 5:30",
+                            "location": "HH-130",
+                            "semester": "Spring 2015"
+                        }
 
-        this.getInfo = function() {
-            return clubInfo
-        }
-    }
-]);
+                    };
+
+                    this.getInfo = function() {
+                        return clubInfo
+                    }
+                }
+            ]);
